b7abcb4f78a82e3775b1b080a59e1109bac06571
Dimitar Nizamov <dimitur2204@gmail.com>
2024-10-05 17:46:16 +0200

    add grpc csharp client

---
+++ csharp/grpc/GrpcClient.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using Via.Dk;
+
+namespace grpc;
+
+public class GrpcClient
+{
+    private readonly TestService.TestServiceClient _client;
+    public GrpcClient()
+    {
+        using var channel = GrpcChannel.ForAddress("http://localhost:8181");
+        this._client = new TestService.TestServiceClient(channel);
+    }
+    public async Task<String> AddTest()
+    {
+        var request = new TestRequest{Name = "Dimitar", Description = "Is clever af"};
+        var reply = await this._client.helloAsync(request);
+        return reply.Greeting; 
+    }
+}
