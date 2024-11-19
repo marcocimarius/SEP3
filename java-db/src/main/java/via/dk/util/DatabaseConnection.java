f32d50ba8d638231d6bbf1c2cf8f544b76f49f3d
marcocimarius <mmarcoci55@gmail.com>
2024-11-19 15:46:47 +0100

    appended database connection setting specific for me

--- java-db/src/main/java/via/dk/util/DatabaseConnection.java
+++ java-db/src/main/java/via/dk/util/DatabaseConnection.java
@@ -8,8 +8,9 @@ public class DatabaseConnection {
 
 	private static Connection con = null;
 	// change based on your database
-	private static final String url = "jdbc:postgresql://localhost:5432/goodbyerotten";
-	private static final String user = "dimitar.nizamov";
+	private static final String url = "jdbc:postgresql://localhost:5432/goodbyerotten"; //dimitar, marius
+	//private static final String user = "dimitar.nizamov"; //dimitar
+	private static final String user = "postgres"; //marius
 	private static final String password = "";
 
 	static
