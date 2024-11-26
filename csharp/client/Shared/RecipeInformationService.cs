2568144be7bb852054589ae026123426ccc31900
Andreea5555 <151520266+Andreea5555@users.noreply.github.com>
2024-11-26 12:41:21 +0100

    trying to add a button

---
+++ csharp/client/Shared/RecipeInformationService.cs
@@ -0,0 +1,25 @@
+﻿namespace DefaultNamespace;
+
+public class RecipeInformationService
+{
+    private string recipeTitle { get; set; }
+    public bool IsButtonClicked { get; set; }
+
+    public void SetButtonClicked()
+    {
+        IsButtonClicked = true;
+    }
+    public void SetButtonNotClicked()
+    {
+        IsButtonClicked = false;
+    }
+
+    public void SetTitle(string title)
+    {
+        recipeTitle = title;
+    }
+    public string GetTitle()
+    {
+        return recipeTitle;
+    }
+}
